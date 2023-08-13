2cb65677880deba14aaa871b884cdd1ac46e2200
zhumos <zhumos@hotmail.com>
2023-08-14 00:39:23 +1000

    Update log4cxx recipe to 1.1.0 and migrate to Conan 2

--- recipes/log4cxx/all/test_package/conanfile.py
+++ recipes/log4cxx/all/test_package/conanfile.py
@@ -1,18 +1,27 @@
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.build.cross_building import cross_building
 import os
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "cmake", "cmake_find_package_multi"
+    generators = "CMakeDeps", "CMakeToolchain"
+    test_type = "explicit"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
 
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
+    def layout(self):
+        cmake_layout(self)
+
     def test(self):
-        if not tools.cross_building(self):
+        if not cross_building(self):
             config_xml_name = os.path.join(self.source_folder, "log4cxx_config.xml")
-            bin_path = os.path.join("bin", "test_package")
-            self.run("{} {}".format(bin_path, config_xml_name), run_environment=True)
+            bin_path = os.path.join(self.cpp.build.bindir, "test_package")
+            self.run("{} {}".format(bin_path, config_xml_name), env="conanrun")
